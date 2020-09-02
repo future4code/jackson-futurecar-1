e5854516bcb7a6575256bad9392be8a0385d0961
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-09-01 22:47:44 -0300

    version 0.1  Card

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ProductCard from './ProductCard/ProductCard'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+        <ProductCard/>
       </div>
     )
   }
