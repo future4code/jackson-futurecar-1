9357ae7b356651f09650b1e07c4cc328acbd5e07
Flavia <afrodrigues38@gmail.com>
2020-09-02 14:29:04 -0300

    Criação e estilização da página inicial

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import InitialPage from './InitialPage/InitialPage'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+       <InitialPage/>
+        
       </div>
     )
   }
