10433f98ccebd88c8c13ea5b0badef70f0fc326f
Flavia <afrodrigues38@gmail.com>
2020-09-03 20:35:13 -0300

    alteração na AppContainer para acertar o conflito

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -3,15 +3,14 @@ import ProductCard from './ProductCard/ProductCard'
 import { PaginaProdutos } from './PaginaProdutos'
 import { RaphaelLayoutTeste } from './RaphaelLayoutTeste'
 import PaginaCadastro from '../components/PaginaCadastro'
+import InitialPage from './InitialPage/InitialPage'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
+        <InitialPage/>
         <ProductCard/>
-
-        
-        
         <RaphaelLayoutTeste></RaphaelLayoutTeste>
         <PaginaCadastro />
       </div>
